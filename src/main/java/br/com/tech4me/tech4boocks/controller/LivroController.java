f76f9e7d9d72ddd9a8b8d0bb3c686b4fea75e049
larahAvel141 <larahavellar44@gmail.com>
2022-02-23 22:14:10 -0300

    Implementando o DeleteMapping

--- src/main/java/br/com/tech4me/tech4boocks/controller/LivroController.java
+++ src/main/java/br/com/tech4me/tech4boocks/controller/LivroController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,9 +47,16 @@ public class LivroController {
         return new ResponseEntity<>(repositorio.save(livro), HttpStatus.CREATED);
     }
 
-    @PutMapping
-    public ResponseEntity<Livro> atualizarLivro(@RequestBody Livro livro){
-        return new ResponseEntity<>(repositorio.save(livro), HttpStatus.CREATED);
+    @PutMapping("/{id}")
+    public ResponseEntity<Livro> atualizarLivro(@PathVariable String id, @RequestBody Livro livro){
+        livro.setId(id);
+        return new ResponseEntity<>(repositorio.save(livro),HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> apagarLivro(@PathVariable String id){
+        repositorio.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     
